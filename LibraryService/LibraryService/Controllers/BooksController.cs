cf235b57b2ff57fa962c076c392db86474c6b161
Mike <mike@ninelinesofcode.com>
2014-04-15 16:12:26 -0700

    Added book controller

---
+++ LibraryService/LibraryService/Controllers/BooksController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using LibraryService.Models;
+using LibraryService.ViewModels;
+
+namespace LibraryService.Controllers
+{
+    public class BooksController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Books
+        public IQueryable<BookViewModel> GetBooks()
+        {
+            return db.Books.Select(b => new BookViewModel { Author = b.Author, Title = b.Title, Id = b.Id });
+        }
+
+        // GET: api/Books/5
+        [ResponseType(typeof(Book))]
+        public IHttpActionResult GetBook(int id)
+        {
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BookExists(int id)
+        {
+            return db.Books.Count(e => e.Id == id) > 0;
+        }
+    }
+}
