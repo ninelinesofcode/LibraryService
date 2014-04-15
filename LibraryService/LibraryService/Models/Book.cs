90646a24c1917cc94af7007e1235f304cf314a10
Mike <mike@ninelinesofcode.com>
2014-04-14 20:32:26 -0700

    Adding Book to model
    Related Work Items: #5

---
+++ LibraryService/LibraryService/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+
+namespace LibraryService.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Title { get; set; }
+
+        public ApplicationUser CheckedOutTo { get; set; }
+    }
+}
