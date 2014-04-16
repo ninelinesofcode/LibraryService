dad9e26a996383568c9bd030c50f425e63a43ff0
Mike <mike@ninelinesofcode.com>
2014-04-15 20:52:38 -0700

    Add Migrate to Latest Version on application start

--- LibraryService/LibraryService/Global.asax.cs
+++ LibraryService/LibraryService/Global.asax.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using LibraryService.Migrations;
+using LibraryService.Models;
 
 namespace LibraryService
 {
@@ -18,6 +21,8 @@ namespace LibraryService
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
         }
     }
 }
