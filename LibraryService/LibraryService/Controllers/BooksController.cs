08feb15182f4ed65faa49f1fe2e2d31211548ca4
Mike <mike@ninelinesofcode.com>
2014-04-15 17:06:04 -0700

    Can get books checked out by a user

--- LibraryService/LibraryService/Controllers/BooksController.cs
+++ LibraryService/LibraryService/Controllers/BooksController.cs
@@ -6,34 +6,59 @@ using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
 using LibraryService.Models;
 using LibraryService.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace LibraryService.Controllers
 {
+    [Authorize]
     public class BooksController : ApiController
     {
         private ApplicationDbContext db = new ApplicationDbContext();
+        private UserManager<ApplicationUser> manager;
+
+        public BooksController()
+        {
+            manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+        }
 
         // GET: api/Books
+        [AllowAnonymous]
         public IQueryable<BookViewModel> GetBooks()
         {
             return db.Books.Select(b => new BookViewModel { Author = b.Author, Title = b.Title, Id = b.Id });
         }
 
         // GET: api/Books/5
-        [ResponseType(typeof(Book))]
-        public IHttpActionResult GetBook(int id)
+        [ResponseType(typeof(IEnumerable<CheckedOutBookViewModel>))]
+        [Route("api/user/books")]
+        public async Task<IHttpActionResult> GetCheckedOutBooks()
         {
-            Book book = db.Books.Find(id);
-            if (book == null)
+            var currentUser = await manager.FindByIdAsync(User.Identity.GetUserId());
+            var booksCheckedOut= currentUser.BooksCheckedOut;
+            
+            if (booksCheckedOut == null)
             {
                 return NotFound();
             }
 
-            return Ok(book);
+            var checkedOutBookViewModel =
+                booksCheckedOut.Select(
+                    b =>
+                        new CheckedOutBookViewModel
+                        {
+                            Author = b.Book.Author,
+                            BookId = b.Book.Id,
+                            Title = b.Book.Title,
+                            UserName = currentUser.UserName
+                        });
+
+            return Ok(checkedOutBookViewModel);
         }
 
         protected override void Dispose(bool disposing)
---
+++ LibraryService/LibraryService/ViewModels/BookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryService.ViewModels
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+    }
+}
