c4e2c74e20117a277927e04092fd803060d21624
Mike <mike@ninelinesofcode.com>
2014-04-16 19:51:00 -0700

    Create BooksService and move BookController logic to it

--- LibraryService/LibraryService/Controllers/BooksController.cs
+++ LibraryService/LibraryService/Controllers/BooksController.cs
@@ -6,10 +6,13 @@ using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
 using LibraryService.Models;
+using LibraryService.Services.Implementation;
 using LibraryService.ViewModels;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -21,17 +24,22 @@ namespace LibraryService.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
         private UserManager<ApplicationUser> manager;
+        private BooksService booksService;
 
         public BooksController()
         {
             manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+
+            booksService = new BooksService(db, manager);
         }
+
+
 
         // GET: api/Books
         [AllowAnonymous]
-        public IQueryable<BookViewModel> GetBooks()
+        public IEnumerable<BookViewModel> GetBooks()
         {
-            return db.Books.Select(b => new BookViewModel { Author = b.Author, Title = b.Title, Id = b.Id });
+            return booksService.GetAllBooks();
         }
 
 
@@ -39,29 +47,12 @@ namespace LibraryService.Controllers
         [Route("api/user/books")]
         public async Task<IHttpActionResult> GetCheckedOutBooks()
         {
-            var currentUser = await manager.FindByIdAsync(User.Identity.GetUserId());
-            var booksCheckedOut = currentUser.BooksCheckedOut;
-
-            if (booksCheckedOut == null)
-            {
-                return NotFound();
-            }
-
-            var checkedOutBookViewModel =
-                booksCheckedOut.Select(
-                    b =>
-                        new CheckedOutBookViewModel
-            {
-                Author = b.Book.Author,
-                BookId = b.Book.Id,
-                Title = b.Book.Title,
-                UserName = currentUser.UserName
-            });
+            var checkedOutBookViewModel = await booksService.GetCheckedOutBooks(User);
 
             return Ok(checkedOutBookViewModel);
         }
 
-        [ResponseType(typeof(IEnumerable<CheckedOutBookViewModel>))]
+        [ResponseType(typeof(CheckedOutBookViewModel))]
         [Route("api/books/{bookId}/checkout/user")]
         [HttpPost]
         public async Task<IHttpActionResult> CheckoutBook(int? bookId)
@@ -71,35 +62,11 @@ namespace LibraryService.Controllers
                 return BadRequest("Invalid bookId");
             }
 
-            var currentUser = await manager.FindByIdAsync(User.Identity.GetUserId());
-            var booksCheckedOut = currentUser.BooksCheckedOut;
+            var checkedOutBook = await booksService.CheckOutBook(bookId.Value, User);
 
-            if (booksCheckedOut != null && booksCheckedOut.Count >= 3)
-            {
-                return BadRequest("The users already has 3 books checked out");
-            }
-
-            var book = await db.Books
-                .FirstOrDefaultAsync(b => b.Id == bookId);
-            if (book == null)
-            {
-                return NotFound();
-            }
-
-            var physicalBook = book.PhysicalBooks
-                .FirstOrDefault(p => p.User == null);
-            if (physicalBook == null)
-            {
-                return BadRequest("Book is already checked out");
-            }
-
-            physicalBook.User = currentUser;
-            await db.SaveChangesAsync();
-
-            return Ok();
+            return Ok(checkedOutBook);
         }
 
-        [ResponseType(typeof(IEnumerable<CheckedOutBookViewModel>))]
         [Route("api/books/{bookId}/checkin/user")]
         [HttpPost]
         public async Task<IHttpActionResult> CheckinBook(int? bookId)
@@ -109,18 +76,7 @@ namespace LibraryService.Controllers
                 return BadRequest("Invalid bookId");
             }
 
-            var userId = User.Identity.GetUserId();
-            var physicalBook = await db.PhysicalBooks
-                .Include(p => p.User)
-                .FirstOrDefaultAsync(b => b.Book.Id == bookId && b.User.Id == userId);
-            if (physicalBook == null)
-            {
-                return NotFound();
-            }
-
-            physicalBook.User = null;
-            await db.SaveChangesAsync();
-
+            await booksService.CheckInBook(bookId.Value, User);
             return Ok();
         }
         protected override void Dispose(bool disposing)
@@ -130,11 +86,6 @@ namespace LibraryService.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
-        }
-
-        private bool BookExists(int id)
-        {
-            return db.Books.Count(e => e.Id == id) > 0;
         }
     }
 }
---
+++ LibraryService/LibraryService/Services/Implementation/BooksService.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Web;
+using LibraryService.ViewModels;
+using LibraryService.Models;
+using Microsoft.AspNet.Identity;
+
+namespace LibraryService.Services.Implementation
+{
+    public class BooksService
+    {
+        private readonly ApplicationDbContext _context;
+
+        private readonly UserManager<ApplicationUser> _manager;
+
+        public BooksService(ApplicationDbContext context, UserManager<ApplicationUser> manager)
+        {
+            _manager = manager;
+            _context = context;
+        }
+
+        public IEnumerable<BookViewModel> GetAllBooks()
+        {
+
+            return _context.Books.Select(b => new BookViewModel { Author = b.Author, Title = b.Title, Id = b.Id }).AsEnumerable();
+        }
+
+        public async Task<IEnumerable<CheckedOutBookViewModel>> GetCheckedOutBooks(IPrincipal user)
+        {
+            var currentUser = await _manager.FindByIdAsync(user.Identity.GetUserId());
+            var booksCheckedOut = currentUser.BooksCheckedOut;
+
+            if (booksCheckedOut == null)
+            {
+                return null;
+            }
+
+            var checkedOutBookViewModel =
+                booksCheckedOut.Select(
+                    b =>
+                        new CheckedOutBookViewModel
+            {
+                Author = b.Book.Author,
+                BookId = b.Book.Id,
+                Title = b.Book.Title,
+                UserName = currentUser.UserName
+            });
+
+            return checkedOutBookViewModel;
+        }
+
+        public async Task<CheckedOutBookViewModel> CheckOutBook(int bookId, IPrincipal user)
+        {
+            var userId = user.Identity.GetUserId();
+
+            var booksCheckedOut = _context.PhysicalBooks
+                .Include(p => p.User)
+                .Where(b => b.Book.Id == bookId && b.User.Id == userId);
+
+            if (booksCheckedOut != null && booksCheckedOut.Count() >= 3)
+            {
+                //return BadRequest("The users already has 3 books checked out");
+                return null;
+            }
+
+            var book = await _context.Books
+
+                .FirstOrDefaultAsync(b => b.Id == bookId);
+            if (book == null)
+            {
+                //return NotFound();
+                return null;
+            }
+
+            var physicalBook = book.PhysicalBooks
+                .FirstOrDefault(p => p.User == null);
+            if (physicalBook == null)
+            {
+                //return BadRequest("Book is already checked out");
+                return null;
+            }
+
+            physicalBook.User = await _manager.FindByIdAsync(userId);
+            await _context.SaveChangesAsync();
+
+            var checkedOutBook = new CheckedOutBookViewModel
+            {
+                Author = book.Author,
+                BookId = book.Id,
+                Title = book.Title,
+                UserName = user.Identity.GetUserName()
+            };
+
+            return checkedOutBook;
+        }
+
+        public async Task CheckInBook(int bookId, IPrincipal user)
+        {
+            var userId = user.Identity.GetUserId();
+            var physicalBook = await _context.PhysicalBooks
+                .Include(p => p.User)
+                .FirstOrDefaultAsync(b => b.Book.Id == bookId && b.User.Id == userId);
+            if (physicalBook == null)
+            {
+                return;
+
+                //return NotFound();
+            }
+
+            physicalBook.User = null;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
